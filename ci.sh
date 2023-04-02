8087741f3f7fc38c4a77fc3f0b325050e1a422f6
Rebecca Ribas <beky.ribas@gmail.com>
2023-04-01 20:11:56 -0400

    missing package

--- ci.sh
+++ ci.sh
@@ -2,6 +2,7 @@
 
 ./download-pkg.sh bee https://github.com/bec-ca/bee/archive/refs/tags/v1.1.0.tar.gz
 ./download-pkg.sh command https://github.com/bec-ca/command/archive/refs/tags/v1.0.1.tar.gz
+./download-pkg.sh yasf https://github.com/bec-ca/yasf/archive/refs/tags/v1.0.0.tar.gz
 
 for file in diffo/*.cpp; do
   echo "Compiling $file..."
